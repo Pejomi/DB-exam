334bbe357670da08dc065a845a7333b3edb7b9e5
PeterNedergaard <78910318+PeterNedergaard@users.noreply.github.com>
2024-05-31 17:26:43 +0200

    Insert data into db
    Inserts data from 'merged_information_clean.csv' into the local MSSQL database defined in 'get_db_conn.py'

---
+++ insert_from_csv.py
@@ -0,0 +1,154 @@
+import pandas as pd
+import time as timer
+from datetime import time
+from decimal import Decimal
+import main
+import utils
+
+
+def load_and_insert(csv_path, table_name, columns, preprocess=None):
+    batch_size = 1000
+    batch_data = []
+
+    # Load CSV file
+    data = pd.read_csv(csv_path, encoding='utf-8', na_filter=False)
+
+    # Optional preprocessing step
+    if preprocess:
+        for col, action in preprocess.items():
+            if action == 'convert_to_int':
+                data[col] = data[col].replace('', '0').astype(float).astype(int)
+                data[col] = data[col].replace(0, None)
+            elif action == 'convert_to_decimal':
+                data[col] = data[col].map(utils.convert_to_decimal)
+                data[col] = data[col].replace(Decimal(0), None)
+            elif action == 'convert_to_time':
+                data[col] = data[col].replace('', time(0, 0).strftime('%H:%M'))
+                data[col] = data[col].apply(utils.to_time)
+                data[col] = data[col].replace(time(0, 0).strftime('%H:%M'), None)
+            elif action is None:
+                data[col] = data[col].replace('', None)
+
+    # Insert DataFrame into SQL Server
+    query = f"INSERT INTO {table_name} ({', '.join(columns.keys())}) VALUES ({', '.join('?' * len(columns))})"
+    for index, row in data.iterrows():
+        batch_data.append([row[col] for col in columns.values()])
+        if len(batch_data) >= batch_size:
+            main.cursor.executemany(query, batch_data)
+            main.cnxn.commit()
+            batch_data = []
+
+    if batch_data:
+        main.cursor.executemany(query, batch_data)
+        main.cnxn.commit()
+
+    main.cnxn.commit()
+
+
+def insert_all_tables():
+    print("Inserting lookup data into the database...")
+    start = timer.time()
+
+    # For Vehicles
+    insert_vehicles_table()
+
+    # For Severities
+    insert_severities_table()
+
+    # For AreaTypes
+    insert_area_types_table()
+
+    # For AgeBands
+    insert_age_bands_table()
+
+    # For Collisions
+    insert_collisions_table()
+
+    # For EnvironmentConditions
+    insert_environment_conditions_table()
+
+    # For Drivers
+    insert_drivers_table()
+
+    main.cursor.close()
+    main.cnxn.close()
+
+    end = timer.time()
+    print(f"Lookup data inserted in {end - start} seconds.")
+
+
+def insert_vehicles_table():
+    load_and_insert(
+        'data/table-csv/vehicles.csv',
+        'Vehicles',
+        {'make': 'make', 'model': 'model', 'type': 'Vehicle_Type', 'model_year': 'Model_Year'},
+        preprocess={'Model_Year': 'convert_to_int', 'make': None, 'model': None, 'Vehicle_Type': None}
+    )
+
+
+def insert_severities_table():
+    load_and_insert(
+        'data/table-csv/severities.csv',
+        'Severities',
+        {'type': 'Accident_Severity'}
+    )
+
+
+def insert_area_types_table():
+    load_and_insert(
+        'data/table-csv/area_types.csv',
+        'AreaTypes',
+        {'type': 'type'}
+    )
+
+
+def insert_age_bands_table():
+    load_and_insert(
+        'data/table-csv/age_bands.csv',
+        'AgeBands',
+        {'band': 'Age_Band_of_Driver'}
+    )
+
+
+def insert_collisions_table():
+    load_and_insert(
+        'data/table-csv/collisions.csv',
+        'Collisions',
+        {'hit_object_in_carriageway': 'Hit_Object_in_Carriageway',
+         'hit_object_off_carriageway': 'Hit_Object_off_Carriageway', 'vehicle_manoeuvre': 'Vehicle_Manoeuvre',
+         'point_of_impact': 'X1st_Point_of_Impact'},
+        preprocess={'Hit_Object_in_Carriageway': None, 'Hit_Object_off_Carriageway': None, 'Vehicle_Manoeuvre': None,
+                    'X1st_Point_of_Impact': None}
+    )
+
+
+def insert_environment_conditions_table():
+    load_and_insert(
+        'data/table-csv/environment_conditions.csv',
+        'EnvironmentConditions',
+        {'light': 'Light_Conditions', 'weather': 'Weather_Conditions', 'road_type': 'Road_Type',
+         'road_surface': 'Road_Surface_Conditions', 'speed_limit': 'Speed_limit', 'area_type_ID': 'area_type_ID'},
+        preprocess={'Light_Conditions': None, 'Weather_Conditions': None, 'Road_Type': None,
+                    'Road_Surface_Conditions': None, 'Speed_limit': 'convert_to_int'}
+    )
+
+
+def insert_drivers_table():
+    load_and_insert(
+        'data/table-csv/drivers.csv',
+        'Drivers',
+        {'age_band_ID': 'age_band_ID', 'sex': 'Sex_of_Driver', 'area_type_ID': 'area_type_ID'},
+        preprocess={'Sex_of_Driver': None}
+    )
+
+
+def insert_accidents_table():
+    load_and_insert(
+        'data/table-csv/accidents.csv',
+        'Accidents',
+        {'vehicle_ID': 'vehicle_ID', 'driver_ID': 'driver_ID', 'severity_ID': 'severity_ID',
+            'environment_conditions_ID': 'environment_conditions_ID', 'collision_ID': 'collision_ID', 'date': 'Date', 'number_of_casualties': 'Number_of_Casualties',
+            'number_of_vehicles': 'Number_of_Vehicles', 'time': 'Time', 'latitude': 'Latitude', 'longitude': 'Longitude'},
+        preprocess={'Number_of_Casualties': 'convert_to_int', 'Number_of_Vehicles': 'convert_to_int',
+                    'Time': 'convert_to_time', 'Latitude': 'convert_to_decimal', 'Longitude': 'convert_to_decimal'}
+    )
--- main.py
+++ main.py
@@ -1,42 +1,159 @@
 import pandas as pd
 import get_db_conn
+import numpy as np
 import sql_queries
 import clean_data
+import time
+import insert_from_csv
+import utils
+
+pd.set_option('display.max_rows', None)
+pd.set_option('display.max_columns', None)
+
+cnxn = get_db_conn.get_db_conn()
+cursor = cnxn.cursor()
+
+
+def insert_lookup_get_id(table, primary_key, columns, values):
+    # Ensure columns and values are in list format for uniform processing
+    if isinstance(columns, str):
+        columns = [columns]
+        values = [values]
+
+    # Replace empty strings or NaN values with None
+    values = [None if v == '' or pd.isna(v) else v for v in values]
+
+    # Build the query components dynamically based on columns count
+    insert_columns = []  # Columns to include in the INSERT statement
+    query_placeholders = []  # Placeholders for values in the INSERT statement
+    where_clause_parts = []
+    insert_params = []  # Parameters for the INSERT and SELECT queries
+
+    for col, val in zip(columns, values):
+        if val is None:
+            where_clause_parts.append(f"{col} IS NULL")
+        else:
+            where_clause_parts.append(f"{col} = ?")
+            insert_columns.append(col)
+            query_placeholders.append('?')
+            insert_params.append(val)
+
+    where_clause = ' AND '.join(where_clause_parts)
+    insert_columns_str = ', '.join(insert_columns)
+    query_placeholders_str = ', '.join(query_placeholders)
+
+    # Check if the entry exists with the exact combination including handling NULLs properly
+    select_query = f"SELECT {primary_key} FROM {table} WHERE {where_clause}"
+    cursor.execute(select_query, tuple(insert_params))
+    result = cursor.fetchone()
+    if result:
+        return result[0]
+    else:
+        # Insert new entry if it does not exist
+        if insert_columns:  # Check if there are any columns to insert
+            insert_query = f"INSERT INTO {table} ({insert_columns_str}) VALUES ({query_placeholders_str})"
+            cursor.execute(insert_query, tuple(insert_params))
+            cnxn.commit()
+        return cursor.execute("SELECT @@IDENTITY").fetchval()
+
+
+def execute_lookup(data_df, row, table, primary_key, columns):
+    if isinstance(columns, str):
+        # Single column, apply directly
+        return data_df[row].apply(lambda x: insert_lookup_get_id(table, primary_key, columns, x))
+    else:
+        # Multiple columns, assume 'row' is a list of column names of equal length to 'columns'
+        return data_df[row].apply(
+            lambda row_values: insert_lookup_get_id(table, primary_key, columns, list(row_values)),
+            axis=1)
 
 
 if __name__ == '__main__':
     # data = pd.read_csv('data/merged_information.csv', encoding='ISO-8859-1')
     # clean_data.clean_data(data)
 
-    # Get DB connection
-    cnxn = get_db_conn.get_db_conn()
-    cursor = cnxn.cursor()
-
-    # Load data
-    data = pd.read_csv('data/merged_information_clean.csv', encoding='utf-8', nrows=100, na_filter=False)
-
-    # Create tables if they don't exist
     sql_queries.create_tables_if_not_exist(cnxn)
 
-    for index, row in data.iterrows():
-        cursor.execute("""INSERT INTO AgeBands (band) OUTPUT INSERTED.age_band_id VALUES (?);""",
-                       row['Age_Band_of_Driver'])
+    # Load data
+    print("Loading data...")
+    start_lookup = time.time()
+    data = pd.read_csv('data/merged_information_clean.csv', nrows=1000, encoding='utf-8', na_filter=False)
 
-        age_band_id = cursor.fetchone()[0]
+    print("Loading existing lookup tables...")
+    existing_age_bands = pd.read_sql("SELECT * FROM AgeBands", cnxn)
+    existing_area_types = pd.read_sql("SELECT * FROM AreaTypes", cnxn)
+    existing_collisions = pd.read_sql("SELECT * FROM Collisions", cnxn)
+    existing_severities = pd.read_sql("SELECT * FROM Severities", cnxn)
+    existing_vehicles = pd.read_sql("SELECT * FROM Vehicles", cnxn)
 
-        cursor.execute("""INSERT INTO AreaTypes (type) OUTPUT INSERTED.area_type_id VALUES (?);""",
-                       row['Driver_Home_Area_Type'])
+    print("Creating lookup dataframes...")
 
-        area_type_id = cursor.fetchone()[0]
+    age_bands = data[~data['Age_Band_of_Driver'].isin(existing_age_bands['band'])][
+        ['Age_Band_of_Driver']].drop_duplicates()
 
-        # cursor.execute("""INSERT INTO Collisions
+    severities = data[~data['Accident_Severity'].isin(existing_severities['type'])][
+        ['Accident_Severity']].drop_duplicates()
 
-        
-        cursor.execute("""
-        INSERT INTO Drivers (age_band, sex, home_area_type) OUTPUT INSERTED.driver_id VALUES (?, ?, ?);""",
-                       row['Age_Band_of_Driver'], row['Sex_of_Driver'], row['Driver_Home_Area_Type'])
+    collisions = utils.get_unique_collision_for_insert(existing_collisions, data)
 
+    data['Driver_Home_Area_Type'] = data['Driver_Home_Area_Type'].replace('Urban area', 'Urban')
+    area_types_array = pd.unique(pd.concat([data['Driver_Home_Area_Type'], data['Urban_or_Rural_Area']]))
+    area_types = pd.DataFrame(area_types_array, columns=['type'])
+    area_types = area_types[~area_types['type'].isin(existing_area_types['type'])]
 
-    cnxn.commit()
+    # Ensure the 'Date' column is in datetime format
+    data['Date'] = pd.to_datetime(data['Date'])
+    # Apply the function to calculate the model year
+    data['Model_Year'] = data.apply(utils.calculate_model_year, axis=1)
+    vehicles = utils.get_unique_vehicles_for_insert(existing_vehicles, data)
+
+    age_bands.to_csv('data/table-csv/age_bands.csv', encoding='utf-8', index=False)
+    severities.to_csv('data/table-csv/severities.csv', encoding='utf-8', index=False)
+    area_types.to_csv('data/table-csv/area_types.csv', encoding='utf-8', index=False)
+    collisions.to_csv('data/table-csv/collisions.csv', encoding='utf-8', index=False)
+    vehicles.to_csv('data/table-csv/vehicles.csv', encoding='utf-8', index=False)
+
+    insert_from_csv.insert_age_bands_table()
+    insert_from_csv.insert_severities_table()
+    insert_from_csv.insert_area_types_table()
+    insert_from_csv.insert_collisions_table()
+    insert_from_csv.insert_vehicles_table()
+
+    end_lookup = time.time()
+    print(f"Lookup dataframes created in {end_lookup - start_lookup} seconds.")
+
+    print("Loading existing lookup tables after insert...")
+    start_remaining = time.time()
+
+    existing_age_bands = pd.read_sql("SELECT * FROM AgeBands", cnxn)
+    existing_area_types = pd.read_sql("SELECT * FROM AreaTypes", cnxn)
+    existing_collisions = pd.read_sql("SELECT * FROM Collisions", cnxn)
+    existing_severities = pd.read_sql("SELECT * FROM Severities", cnxn)
+    existing_vehicles = pd.read_sql("SELECT * FROM Vehicles", cnxn)
+    existing_environment_conditions = pd.read_sql("SELECT * FROM EnvironmentConditions", cnxn)
+    existing_drivers = pd.read_sql("SELECT * FROM Drivers", cnxn)
+    existing_accidents = pd.read_sql("SELECT * FROM Accidents", cnxn)
+
+    print("Creating remaining tables for insert...")
+    environment_conditions = utils.get_unique_environment_conditions_for_insert(existing_environment_conditions.copy(deep=True), existing_area_types.copy(deep=True), data)
+    environment_conditions.to_csv('data/table-csv/environment_conditions.csv', encoding='utf-8', index=False)
+    insert_from_csv.insert_environment_conditions_table()
+
+    drivers = utils.get_unique_drivers_for_insert(existing_drivers.copy(deep=True), existing_age_bands.copy(deep=True), existing_area_types.copy(deep=True), data)
+    drivers.to_csv('data/table-csv/drivers.csv', encoding='utf-8', index=False)
+    insert_from_csv.insert_drivers_table()
+
+    existing_environment_conditions = pd.read_sql("SELECT * FROM EnvironmentConditions", cnxn)
+    existing_drivers = pd.read_sql("SELECT * FROM Drivers", cnxn)
+
+    accidents = utils.get_unique_accidents_for_insert(existing_accidents, existing_vehicles, existing_drivers, existing_severities, existing_environment_conditions, existing_collisions, existing_area_types, existing_age_bands, data)
+    accidents.to_csv('data/table-csv/accidents.csv', encoding='utf-8', index=False)
+    insert_from_csv.insert_accidents_table()
+
+    end_remaining = time.time()
+    print(f"Remaining tables inserted in {end_remaining - start_remaining} seconds.")
+
+    print("Operation took a total of", end_remaining - start_lookup, "seconds.")
+
     cursor.close()
     cnxn.close()
