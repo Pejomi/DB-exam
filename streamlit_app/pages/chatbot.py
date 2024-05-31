028adff4481d7d9b5b8e3a7444302bd3b914941f
MieThorberg <cph-mt373@cphbusiness.dk>
2024-05-31 10:23:36 +0200

    chat and sql updated

--- streamlit_app/pages/chatbot.py
+++ streamlit_app/pages/chatbot.py
@@ -6,7 +6,6 @@ from pymongo import MongoClient
 from dotenv import load_dotenv
 import os
 
-
 load_dotenv()
 
 st.set_page_config(page_title="Chatbot", page_icon="🤖")
@@ -25,12 +24,13 @@ def insert_chat_history(chat_history):
 def response_generator(): # TODO: Implement a real chatbot
     response = random.choice(
         [
+            "How can I help you today?",
+            "What can I do for you today?",
+            "I'm sorry, I don't understand. Can you rephrase that?",
             "Hello there! How can I assist you today?",
-            "Hi, human! Is there human! Is there anything I can help you with? Hi, human! Is there anything I can help you with? Hi, human! Is there anything I can help you with? Hi, human! Is there anything I can help you with? ",
             "Do you need help?",
         ]
     )
-
     # Emulate delay
     for word in response.split():
         yield word + " "
@@ -39,7 +39,7 @@ def response_generator(): # TODO: Implement a real chatbot
 # Login form to get user email before starting chat
 def setup():
     st.info('To start using the chatbot, please login with your email address below. Your email address will be used to save your chat history.')
-    form = st.form(key='my-form')
+    form = st.form(key='login_form')
     name = form.text_input(label="Enter email:", placeholder="Type here ...")
     submit = form.form_submit_button(label='Login')
     if submit:
@@ -50,22 +50,31 @@ def login(email):
         st.error("Please enter a valid email address.")
         return
     st.session_state.email = email
-    st.session_state.messages = []
+    st.session_state.messages = [[], []] # index 0 for new chat, index 1 for saved chat
     st.rerun()
 
-def find_all_user_titles(email):
+def find_all_user_titles():
     collection = connect_to_mongodb()
-    result = collection.find({"email": email})  
+    result = collection.find({"email": st.session_state.email})  
     return result
 
-def find_user_chat(email, title):
+def find_user_chat(title):
     collection = connect_to_mongodb()
-    result = collection.find({"email": email, "title": title})  
+    result = collection.find({"email": st.session_state.email, "title": title})  
     return result
 
-def reset_chat():
-    st.session_state.messages = []
+def clear_chat():
+    st.session_state.messages[0] = []
     st.rerun()
+
+def delete_chat(title):
+    collection = connect_to_mongodb()
+    collection.delete_one({"email": st.session_state.email, "title": title})
+    st.rerun()
+
+def update_chat(title):
+    collection = connect_to_mongodb()
+    collection.update_one({"email": st.session_state.email, "title": title}, {"$set": {"messages": st.session_state.messages[1]}})
 
 st.markdown(
     """
@@ -75,63 +84,99 @@ st.markdown(
                 text-align: justify;
                 text-align-last: right;
                 -moz-text-align-last: right;
-                background-color: transparent;
+            }
+            .st-emotion-cache-1c7y2kd {
+                flex-direction: row-reverse;
+                text-align: justify;
+                text-align-last: right;
+                -moz-text-align-last: right;
             }
             .st-emotion-cache-4oy321 {
-                background-color: rgba(38, 39, 48, 0.5);
                 padding: 1rem;
+            }
+             .st-emotion-cache-13ln4jf {
+                height: 100vh;
             }
         </style>
     """,
     unsafe_allow_html=True,
 )
-if "email" not in st.session_state or st.session_state.email is None:
-        setup()
+
+if "email" not in st.session_state:
+    setup() # Login setup
 else:
     tab1, tab2 = st.tabs(["Chat", "Saved messages"])
-    with tab1:
-        st.session_state.chat_open = True
 
-    with tab2:
-        title = st.selectbox("Select chat:", find_all_user_titles(st.session_state.email), placeholder="No chat selected", index=None)
-   
-
-if "chat_open" in st.session_state and st.session_state.chat_open == True:
-# Initialize chat history
-    messages = st.container()
-    with messages:
-        col1, col2 = st.columns([9, 1.25])
+    with tab1: # Chat tab
+        messages = st.session_state.messages[0] # Current chat messages
+        # Save and clear buttons
+        col1, col2, col3 = st.columns([9, 1.45, 1.25])
         with col2:
             with st.popover("Save"):
                 st.write("Save in Mongo DB 📂")
-                name = st.text_input("Name your chat-history:", placeholder="Type here ...")
-                save = st.button("Save")
-                if save:
-                    history = {"email": st.session_state.email, "title": name, "messages": st.session_state.messages}
+                name = st.text_input("Name your chat-history:", placeholder="Type here ...") 
+                if st.button("Save"):
+                    history = {"email": st.session_state.email, "title": name, "messages": messages}
                     insert_chat_history(history)
+        with col3:
+            if st.button("Clear", type="primary"):
+                clear_chat()
+        # Display chat messages
+        tab_container = tab1.container(height=645, border=False)
+        with tab_container:
+            # First message from chatbot
+            first_message = "Hello! How can I help you today?"
+            if "assistant" not in [message["role"] for message in messages]:
+                messages.append({"role": "assistant", "content": first_message})
+            #messages.append({"role": "assistant", "content": first_message})
+            
+            # Display chat messages
+            for message in messages:
+                with tab_container.chat_message(message["role"]):
+                    st.markdown(message["content"])
+        # Chat message input
+        if prompt := st.chat_input("Type your message",key="new_chat"):
+            # Save user message to session state
+            messages.append({"role": "user", "content": prompt})
+            with tab_container.chat_message("user"):
+                st.markdown(prompt)
+            # Generate chatbot response and save to session state
+            with tab_container.chat_message("assistant"):
+                response = st.write_stream(response_generator())
+            messages.append({"role": "assistant", "content": response})
 
-        # Chat first message
-        messages.chat_message("assistant").write("Hello! How can I help you today?")
+    with tab2: # Saved messages tab
+        if len([chat["title"] for chat in find_all_user_titles()]) == 0:
+            st.info("No saved chat history found.")
+            st.stop()
+        else:
+            col1, col2 = st.columns([8,1])
+            with col1:
+                # Select saved chat to be displayed
+                chat = st.selectbox("Select chat:", [chat["title"] for chat in find_all_user_titles()], placeholder="No chat selected", index=None)
+                if chat:
+                    st.session_state.messages[1] = list(find_user_chat(chat))[0]["messages"]
+                else:
+                    st.session_state.messages[1] = []
+            with col2:
+                # Delete selected chat
+                col2.markdown("<div style='width: 1px; height: 29px'></div>", unsafe_allow_html=True)
+                if st.button("Delete", type="primary", disabled=not chat):
+                    delete_chat(chat)
 
-        # Display chat messages from history on app rerun
-        for message in st.session_state.messages:
-            with st.chat_message(message["role"]):
-                st.markdown(message["content"])
-    
-    # Accept user input
-    if prompt := st.chat_input("Type your message"):
-        # Add user message to chat history
-        st.session_state.messages.append({"role": "user", "content": prompt})
-        # Display user message in chat message container
-        with st.chat_message("user"):
-            st.markdown(prompt)
+            # Display selected chat
+            messages = st.session_state.messages[1] # Saved chat messages
+            tab_container = tab2.container(height=615, border=False)
+            with tab_container:
+                for message in messages:
+                    with tab_container.chat_message(message["role"]):
+                        st.markdown(message["content"])
 
-        # Display assistant response in chat message container
-        with st.chat_message("assistant"):
-            response = st.write_stream(response_generator())
-        # Add assistant response to chat history
-        st.session_state.messages.append({"role": "assistant", "content": response})
-
-# Todo:
-# - show selectbox with options of previous chats or create new chat with new title
-# - show chat history
+            if prompt := st.chat_input("Type your message", key="saved_chat", disabled=not chat):
+                messages.append({"role": "user", "content": prompt})
+                with tab_container.chat_message("user"):
+                    st.markdown(prompt)
+                with tab_container.chat_message("assistant"):
+                    response = st.write_stream(response_generator())
+                messages.append({"role": "assistant", "content": response})
+                update_chat(chat) # Update the saved chat history
--- streamlit_app/pages/relational db.py
+++ streamlit_app/pages/relational db.py
@@ -5,34 +5,143 @@ import datetime
 
 st.set_page_config(page_title="Relational DB", page_icon="🚦")
 
-st.header("Relational Database", divider='rainbow')
+tab1, tab2, tab3 = st.tabs(["Search", "Analyze", "🔥Monitoring"])
 
-with st.expander("Table"):
-    df = pd.DataFrame(np.random.randn(50, 20), columns=("col %d" % i for i in range(20)))
-    st.dataframe(df)
+def search():
+    return "search"
 
-options = st.multiselect(
-    "Select values:",
-    ["Green", "Yellow", "Red", "Blue"],
-    )
+with tab1: # Search tab
+    today = datetime.datetime.now()
+    next_year = today.year + 1
+    jan_1 = datetime.date(next_year, 1, 1)
+    dec_31 = datetime.date(next_year, 12, 31)
+    st.info("Search for data about traffical accidents in UK using our **relational database**.")
+    
+    search_form = st.form(key="search_form")
+    with search_form:
+        st.write("Select the search criteria below:")
+        with st.expander("Accidents"):
+            col1, col2 = st.columns(2)
+            with col1:
+                options = st.multiselect(
+                    "Sex1:",
+                    ["Male", "Female", "Both", "Unknown"],
+                )
+                options = st.multiselect(
+                    "Age bands:",
+                    ["16-24", "25-34", "35-44", "45-54", "55-64", "65-74", "75+", "Unknown"],
+                )
+            with col2:
+                options = st.multiselect(
+                    "Severity:",
+                    ["Male", "Female", "Both", "Unknown"],
+                )
+                options = st.multiselect(
+                    "Area:",
+                    ["Male", "Female", "Both", "Unknown"],
+                )
+        with st.expander("Drivers involved in accidents"):
+            col1, col2 = st.columns(2)
+            with col1:
+                options = st.multiselect(
+                    "Sex:",
+                    ["Male", "Female", "Both", "Unknown"],
+                )
+            with col2:
+                option = st.selectbox("Age bands:",("Male", "Female", "Both"), index=None, placeholder="Select contact method...",)               
+        with st.expander("Environment conditions"):
+            col1, col2 = st.columns(2)
+            with col1:
+                options = st.multiselect(
+                    "Weather:",
+                    ["Fine no high winds", "Raining no high winds", "Snowing no high winds", "Fine + high winds", "Raining + high winds", "Snowing + high winds", "Fog or mist", "Other", "Unknown"],
+                )
+            with col2:
+                options = st.multiselect(
+                    "Light conditions:",
+                    ["Daylight", "Darkness - lights lit", "Darkness - lights unlit", "Darkness - no lighting", "Darkness - lighting unknown", "Other", "Unknown"],
+                )
+        date = st.date_input(
+            "Select datetime-stamp:",
+            (jan_1, datetime.date(next_year, 1, 7)),
+            jan_1,
+            dec_31,
+            format="MM.DD.YYYY",
+        )
+        
+        start_color, end_color = st.select_slider(
+            "Select a range of rows:",
+            options=[i for i in range(0, 1201)],
+            value=(0, 1200))
 
-option = st.selectbox(
-    "Choose a method:",
-    ("Create", "Read", "Update", "Delete"), index=None, placeholder="Select contact method...",)
+        columns = st.multiselect(
+                    "Choose columns:",
+                    ["driver", "age", "location"]
+                )
+        submit = search_form.form_submit_button("Search", type="primary")
+    if submit:
+        st.write("Search results:")
+        
+        with st.expander("✅ Result", expanded=True):
+            df = pd.DataFrame(np.random.randn(50, 20), columns=("col %d" % i for i in range(20)))
+            st.dataframe(df)
+        st.error("No result found. Please refine your search criteria.")
 
-st.button("Reset", type="primary")
-st.button("Say hello")
+with tab2: # Analyze tab
+    with st.expander("Drivers involved in accidents"):
+        st.info("put some data here...")
+    with st.expander("Environment conditions"):
+        st.info("put some data here...")
+    with st.expander("Locations", expanded=True):
+        st.info("Click on the points to see information about the accident.")
+        df = pd.DataFrame(np.random.randn(1000, 2) / [50, 50] + [37.76, -122.4], columns=['lat', 'lon'])
+        st.map(df, size=2, use_container_width=False)
 
-today = datetime.datetime.now()
-next_year = today.year + 1
-jan_1 = datetime.date(next_year, 1, 1)
-dec_31 = datetime.date(next_year, 12, 31)
+with tab3: # Monitoring tab
+    st.write("Monitoring")
 
-d = st.date_input(
-    "Select your vacation for next year",
-    (jan_1, datetime.date(next_year, 1, 7)),
-    jan_1,
-    dec_31,
-    format="MM.DD.YYYY",
-)
-d
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    #st.write("Home")
+    #with st.expander("Table"):
+    #    df = pd.DataFrame(np.random.randn(50, 20), columns=("col %d" % i for i in range(20)))
+    #    st.dataframe(df)
+
+    #options = st.multiselect(
+    #    "Select values:",
+    #    ["Green", "Yellow", "Red", "Blue"],
+    #)
+
+    #option = st.selectbox("Choose a method:",("Create", "Read", "Update", "Delete"), index=None, placeholder="Select contact method...",)
+
+    #st.button("Reset", type="primary")
+    #st.button("Say hello")
+
+    #today = datetime.datetime.now()
+    #next_year = today.year + 1
+    #jan_1 = datetime.date(next_year, 1, 1)
+    #dec_31 = datetime.date(next_year, 12, 31)
+
+    #d = st.date_input(
+    #    "Select your vacation for next year",
+    #    (jan_1, datetime.date(next_year, 1, 7)),
+    #    jan_1,
+    #    dec_31,
+    #    format="MM.DD.YYYY",
+    #)
+    #d
+
+
+
